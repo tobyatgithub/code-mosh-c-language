06f4de6443c04ec497579326110dfc1c109ec420
tobyatgithub <toby.fangyuan@gmail.com>
2022-10-27 14:04:44 -0700

    update readme. add a few pointer and two sum script.

--- DataTypes/main.cpp
+++ DataTypes/main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
+vector<int> twoSum(vector<int>& nums, int target){
+    unordered_map<int,int> visited;
+    for (int i = 0; i < nums.size(); i++) {
+        int remain = target - nums[i];
+        if(visited.count(remain)) {
+            return {visited[remain], i};
+        }
+        visited[nums[i]] = i;
+    }
+    return {};
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int x = 10;
+    int* ptr = &x;
+
+    int* numbers = new int[10];
+
+
+
+
+    //     vector<int> ret;
+
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         int remain = target - nums[i];
+    //         for (int j = i; j < nums.size(); j++) {
+    //             if (nums[j] == remain) {
+    //                 ret.push_back(i);
+    //                 ret.push_back(j);
+    //                 return ret;
+    //             }
+    //         }
+    //     }
+    // }
 }
